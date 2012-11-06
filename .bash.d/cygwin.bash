9f1651f54034ba809df196a4338fe365b7adf848
root <root@localhost.localdomain>
2012-11-06 11:34:27 -0500

    cygwin bash

--- .bash.d/cygwin.bash
+++ .bash.d/cygwin.bash
@@ -1,7 +1,9 @@
 ## windows cygwin .bashrc
-#alias desk='cd /cygdrive/c/Users/k_sugi/Desktop'
-#alias down='cd /cygdrive/c/Users/k_sugi/Downloads'
-#alias re='cd /cygdrive/c/pleiades/xampp/htdocs/rehouse'
-#alias cs='cd /cygdrive/c/xampp/htdocs/cs/'
-#alias xamp='cd /cygdrive/c/xampp/'
-alias open='cygstart'
+if [ "$OS" = "Windows NT" ]; then
+	alias desk='cd /cygdrive/c/Users/k_sugi/Desktop'
+	alias down='cd /cygdrive/c/Users/k_sugi/Downloads'
+	alias re='cd /cygdrive/c/pleiades/xampp/htdocs/rehouse'
+	alias cs='cd /cygdrive/c/xampp/htdocs/cs/'
+	alias xamp='cd /cygdrive/c/xampp/'
+	alias open='cygstart'
+fi
