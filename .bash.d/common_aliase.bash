7c4113721d6efa6844c886c1e1d4465239170450
root <root@localhost.localdomain>
2012-11-06 11:58:19 -0500

    windows NT -> Windows_NT

--- .bash.d/common_aliase.bash
+++ .bash.d/common_aliase.bash
@@ -1,6 +1,6 @@
 
 ## windows cygwin .bashrc
-if [ "$OS" = "Windows NT" ]; then
+if [ "$OS" = "Windows_NT" ]; then
 	alias desk='cd /cygdrive/c/Users/k_sugi/Desktop'
 	alias down='cd /cygdrive/c/Users/k_sugi/Downloads'
 	alias re='cd /cygdrive/c/pleiades/xampp/htdocs/rehouse'
