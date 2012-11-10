ddfcd3a345a0a07885a0111376f4023ad73b1ec5
kohei sugi <kohei.sg@gmail.com>
2012-11-10 23:41:54 +0900

    my first commit

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -2,7 +2,7 @@
 alias ll='ls -Gltr'
 alias la='ls -Gltra'
 
-alias cd='pushd'
+alias pd='pushd'
 alias bd='popd'
 
 alias down='cd $HOME/Downloads/'
@@ -12,7 +12,7 @@ alias site='cd $HOME/Sites/'
 alias reps='cd $HOME/Sites/ReportShair/'
 
 alias updatedb='sudo /usr/libexec/locate.updatedb'
-alias real='cat ~/reality'
+alias real='cat ~/.reality'
 
 #git
 alias gst='git status'
@@ -20,10 +20,3 @@ alias gst='git status'
 #tmux
 alias ta='tmux a'
 alias tmux='tmux -u'
-
-#ssh
-alias sshsakura='ssh 219.94.228.51 -l kohei-sugi'
-alias sshharasawa='ssh 03-3893-6004.sakura.ne.jp -l 03-3893-6004'
-alias sshu='ssh koheisg@192.168.56.101'
-alias sshrocket='ssh kohei-sugi@114.179.231.53'
-
