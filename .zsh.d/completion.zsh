48f114fba62a2e03860bd53964bc4e50e5ec61f8
kohei sugi <kohei.sg@gmail.com>
2013-04-04 18:09:02 +0900

    zsh completion

---
+++ .zsh.d/completion.zsh
@@ -0,0 +1,16 @@
+## http://qiita.com/items/ed2d36698a5cc314557d
+autoload -U compinit
+compinit
+zstyle ':completion:*:default' menu select=2
+
+# 補完関数の表示を強化する
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+zstyle ':completion:*:messages' format '%F{YELLOW}%d'$DEFAULT
+zstyle ':completion:*:warnings' format '%F{RED}No matches for:''%F{YELLOW} %d'$DEFAULT
+zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'$DEFAULT
+
+# マッチ種別を別々に表示
+zstyle ':completion:*' group-name ''
