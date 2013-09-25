35a9e8c0b94104f661cde3f0ef7f45fc1b23c5e1
koheisg <kohei.sg@gmail.com>
2013-09-25 15:19:03 +0900

    edit alias of zsh for OS type

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -1,17 +1,3 @@
-#file
-alias ll='ls -Gltr'
-alias la='ls -Gltra'
-
-alias pd='pushd'
-alias bd='popd'
-
-alias down='cd $HOME/Downloads/'
-alias desk='cd $HOME/Desktop/'
-alias pict='cd $HOME/Pictures/'
-alias site='cd $HOME/Sites/'
-alias reps='cd $HOME/Sites/ReportShair/'
-
-alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias real='cat ~/.reality'
 
 #git
@@ -20,3 +6,33 @@ alias gst='git status -s -b'
 #tmux
 alias ta='tmux a'
 alias tmux='tmux -u'
+
+#php
+alias phpdate='php -r "echo(date(\"Y-m-d H:i:s\"));echo(\"\n\");"'
+
+case ${OSTYPE} in
+    darwin*)
+        alias updatedb='sudo /usr/libexec/locate.updatedb'
+        alias down='cd $HOME/Downloads/'
+        alias desk='cd $HOME/Desktop/'
+        alias pict='cd $HOME/Pictures/'
+        alias site='cd $HOME/Sites/'
+
+        #file
+        alias ll='ls -Gltr'
+        alias la='ls -Gltra'
+        ;;
+    linux*)
+        ;;
+    cygwin*)
+        #file
+        alias ll='ls -ltr --color'
+        alias la='ls -ltra --color'
+        alias open='cygstart'
+
+        alias mysql='mysql -h127.0.0.1'
+        alias ta='screen'
+
+        alias vi='vim'
+        ;;
+esac
